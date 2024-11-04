02c3f596186715aa73200983227a460d902339f9
PillowGit <goldentaco727@gmail.com>
2024-11-04 11:25:59 -0800

    Added new utils

--- src/utils/parsing.py
+++ src/utils/parsing.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+def read(file: Path) -> str:
+  with file.open() as f:
+    return f.read().strip()
+
 def lines(file: Path) -> list[str]:
   with file.open() as f:
     return f.read().strip().split('\n')
