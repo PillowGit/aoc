fe57e0aaef1f9f6b221f7505f9b41d69aa7acc63
PillowGit <goldentaco727@gmail.com>
2024-11-30 20:50:40 -0800

    Commented parsing files, added grids

--- src/utils/parsing.py
+++ src/utils/parsing.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
+# Basic reading
+
 def read(file: Path) -> str:
   with file.open() as f:
     return f.read().strip()
+
+# Lines
 
 def lines(file: Path) -> list[str]:
   with file.open() as f:
@@ -11,3 +15,13 @@ def lines(file: Path) -> list[str]:
 def lines_as_ints(file: Path) -> list[int]:
   with file.open() as f:
     return [int(x) for x in f.read().strip().split('\n')]
+
+# Grids
+
+def grid(file: Path) -> list[list[str]]:
+  with file.open() as f:
+    return [list(x) for x in f.read().strip().split('\n')]
+
+def grid_as_ints(file: Path) -> list[list[int]]:
+  with file.open() as f:
+    return [[int(y) for y in x] for x in f.read().strip().split('\n')]
