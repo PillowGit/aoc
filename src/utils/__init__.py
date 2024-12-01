860d0f6ed334362da74ba54bd8386deb4e608fce
PillowGit <goldentaco727@gmail.com>
2024-11-30 21:08:16 -0800

    Added ranges section to utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,2 @@
 from . import parsing
+from . import ranges
---
+++ src/utils/ranges.py
@@ -0,0 +1,7 @@
+def find_overlap(start1: int, end1: int, start2: int, end2: int) -> tuple[int, int] | None:
+  maxStart = max(start1, start2)
+  minEnd = min(end1, end2)
+  if maxStart <= minEnd:
+    return (maxStart, minEnd)
+  else:
+    return None
