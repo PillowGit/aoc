3dbc64e70bfa0b14a457c101627f99b31130a415
PillowGit <goldentaco727@gmail.com>
2024-12-03 16:37:00 -0800

    Added comments to ranges

--- src/utils/ranges.py
+++ src/utils/ranges.py
@@ -1,3 +1,5 @@
+# Returns the overlap given two ranges
+
 def find_overlap(start1: int, end1: int, start2: int, end2: int) -> tuple[int, int] | None:
   maxStart = max(start1, start2)
   minEnd = min(end1, end2)
