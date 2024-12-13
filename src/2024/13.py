671d8127d1b395383463a3fe3591b16d26c8caee
PillowGit <goldentaco727@gmail.com>
2024-12-12 23:07:29 -0800

    Day 13

--- src/2024/13.py
+++ src/2024/13.py
@@ -2,36 +2,34 @@ from pathlib import Path
 from .. import utils as u
 from rich import print
 
-from sys import setrecursionlimit
-setrecursionlimit(10**6)
+import regex as re
+import numpy as np
 
 def sln1(input):
   ans = 0
-  button_a_movement = 0
-  button_b_movement = 0
-  button_location = 0
-  parse_step = 0
-  # Solving
-  def solve_claw_machine(x, y):
-  # Parsing
-  def getints(string):
-      coords = string.replace(' ','').split(':')[1].split(',')
-      return (int(coords[0][2:]), int(coords[1][2:]))
-  for line in input:
-    if parse_step == 0:
-      button_a_movement = getints(line)
-    elif parse_step == 1:
-      button_b_movement = getints(line)
-    elif parse_step == 2:
-      button_location = getints(line)
-    else:
-      ans += solve_claw_machine(0, 0)
-      parse_step = -1
-    parse_step += 1
+  chunks = re.split(r'\n\n', input)
+  for chunk in chunks:
+    ai, aj, bi, bj, ti, tj = map(int, re.findall(r'\d+', chunk))
+    l = np.array([[ai, bi], [aj, bj]])
+    r = np.array([ti, tj])
+    a_presses, b_presses = map(round, np.linalg.solve(l, r))
+    if a_presses*ai + b_presses*bi == ti and a_presses*aj + b_presses*bj == tj:
+      ans += a_presses*3 + b_presses
   return ans
 
 def sln2(input):
-  return -1
+  ans = 0
+  chunks = re.split(r'\n\n', input)
+  for chunk in chunks:
+    ai, aj, bi, bj, ti, tj = map(int, re.findall(r'\d+', chunk))
+    ti += 10000000000000
+    tj += 10000000000000
+    l = np.array([[ai, bi], [aj, bj]])
+    r = np.array([ti, tj])
+    a_presses, b_presses = map(round, np.linalg.solve(l, r))
+    if a_presses*ai + b_presses*bi == ti and a_presses*aj + b_presses*bj == tj:
+      ans += a_presses*3 + b_presses
+  return ans
 
 if __name__ == '__main__':
   this_file = Path(__file__).resolve()
@@ -39,7 +37,7 @@ if __name__ == '__main__':
   input_file = this_file.parent / f'{day}.txt'
   
   # Use any parsing method here
-  parsed_input = u.parsing.lines(input_file)
+  parsed_input = u.parsing.read(input_file)
 
   print('Part 1:', sln1(parsed_input))
   print('Part 2:', sln2(parsed_input))
